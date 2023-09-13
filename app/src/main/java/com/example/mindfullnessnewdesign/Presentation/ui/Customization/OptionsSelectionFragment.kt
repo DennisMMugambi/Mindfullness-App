7967bde569dbe9f52508395b74897511fd7d716c
Dennis Mugambi <dennis.mutwiri.mugambi@gmail.com>
2023-09-13 15:44:22 +0300

    adds options selection fragment

---
+++ app/src/main/java/com/example/mindfullnessnewdesign/Presentation/ui/Customization/OptionsSelectionFragment.kt
@@ -0,0 +1,19 @@
+package com.example.mindfullnessnewdesign.Presentation.ui.Customization
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.example.mindfullnessnewdesign.R
+import com.example.mindfullnessnewdesign.databinding.FragmentOptionsSelectionBinding
+
+class OptionsSelectionFragment : Fragment(R.layout.fragment_options_selection) {
+
+    private lateinit var binding: FragmentOptionsSelectionBinding
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        binding = FragmentOptionsSelectionBinding.bind(view)
+    }
+}
