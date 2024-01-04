8b6ce725bc1006313878c541115c331d37ec312f
Dennis Mugambi <dennismugambi@Denniss-MacBook-Pro.local>
2024-01-04 15:58:39 +0300

    completes meditate fragment design

--- app/src/main/java/com/example/mindfullnessnewdesign/Presentation/ui/Meditate/MeditateFragment.kt
+++ app/src/main/java/com/example/mindfullnessnewdesign/Presentation/ui/Meditate/MeditateFragment.kt
@@ -5,7 +5,12 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.example.mindfullnessnewdesign.Domain.Models.MeditationItem
+import com.example.mindfullnessnewdesign.Presentation.adapters.GenericAdapter
 import com.example.mindfullnessnewdesign.R
+import com.example.mindfullnessnewdesign.databinding.DailyExercisesLayoutBinding
 import com.example.mindfullnessnewdesign.databinding.FragmentMeditateBinding
 import dagger.hilt.android.AndroidEntryPoint
 
@@ -13,9 +18,111 @@ import dagger.hilt.android.AndroidEntryPoint
 class MeditateFragment : Fragment(R.layout.fragment_meditate) {
 
     private lateinit var binding: FragmentMeditateBinding
+
+
+    private lateinit var recyclerView : RecyclerView
+    private lateinit var mAdapter: GenericAdapter<MeditationItem>
+
+    val meditationList = listOf(
+        MeditationItem(
+            meditationImage = R.drawable.floral_2, // Replace with the actual image resource ID
+            meditationTitle = "Basic",
+            meditationDescription = "Learn the fundamentals of meditation",
+            meditationCourse = "Beginner's Guide",
+            meditationDuration = "15 minutes"
+        ),
+        MeditationItem(
+            meditationImage = R.drawable.floral_2, // Replace with the actual image resource ID
+            meditationTitle = "Basic",
+            meditationDescription = "Learn the fundamentals of meditation",
+            meditationCourse = "Beginner's Guide",
+            meditationDuration = "15 minutes"
+        ),
+        MeditationItem(
+            meditationImage = R.drawable.floral_2, // Replace with the actual image resource ID
+            meditationTitle = "Basic",
+            meditationDescription = "Learn the fundamentals of meditation",
+            meditationCourse = "Beginner's Guide",
+            meditationDuration = "15 minutes"
+        ),
+        MeditationItem(
+            meditationImage = R.drawable.floral_2, // Replace with the actual image resource ID
+            meditationTitle = "Basic",
+            meditationDescription = "Learn the fundamentals of meditation",
+            meditationCourse = "Beginner's Guide",
+            meditationDuration = "15 minutes"
+        )
+    )
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
         binding = FragmentMeditateBinding.bind(view)
+
+        setUpAll(meditationList)
+        setUpQuicknEasy(meditationList)
+    }
+
+    private fun setUpAll(list: List<MeditationItem>) {
+
+        mAdapter = GenericAdapter()
+        recyclerView = binding.allRecycler
+        recyclerView.layoutManager = LinearLayoutManager(requireContext(), LinearLayoutManager.HORIZONTAL, false)
+        recyclerView.adapter = mAdapter
+//        recyclerView.apply {
+//            layoutManager = LinearLayoutManager(context).apply { LinearLayoutManager.HORIZONTAL }
+//            adapter = mAdapter
+//        }
+
+        mAdapter.listOfItems = list
+
+        mAdapter.expressionOnCreateViewHolder = { viewGroup ->
+            DailyExercisesLayoutBinding.inflate(
+                LayoutInflater.from(viewGroup.context),
+                viewGroup,
+                false
+            )
+        }
+
+        mAdapter.expressionViewHolderBinding = { item, viewBinding ->
+            val view = viewBinding as DailyExercisesLayoutBinding
+
+            // Bind data to views
+            view.title.text = item.meditationTitle
+            view.descriptionText.text = item.meditationDescription
+            view.topImage.setImageResource(item.meditationImage)
+        }
+
+    }
+
+    private fun setUpQuicknEasy(list: List<MeditationItem>) {
+
+        mAdapter = GenericAdapter()
+        recyclerView = binding.easyItemsRecycler
+        recyclerView.layoutManager = LinearLayoutManager(requireContext(), LinearLayoutManager.HORIZONTAL, false)
+        recyclerView.adapter = mAdapter
+//        recyclerView.apply {
+//            layoutManager = LinearLayoutManager(context).apply { LinearLayoutManager.HORIZONTAL }
+//            adapter = mAdapter
+//        }
+
+        mAdapter.listOfItems = list
+
+        mAdapter.expressionOnCreateViewHolder = { viewGroup ->
+            DailyExercisesLayoutBinding.inflate(
+                LayoutInflater.from(viewGroup.context),
+                viewGroup,
+                false
+            )
+        }
+
+        mAdapter.expressionViewHolderBinding = { item, viewBinding ->
+            val view = viewBinding as DailyExercisesLayoutBinding
+
+            // Bind data to views
+            view.title.text = item.meditationTitle
+            view.descriptionText.text = item.meditationDescription
+            view.topImage.setImageResource(item.meditationImage)
+        }
+
     }
 }
